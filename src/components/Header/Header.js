89338eae377d8b9012128ab24a781327d475caf4
Nizii <nizam.oezdemir@gmx.ch>
2023-08-27 15:21:14 +0200

    Adresszeile über Header hinzugefügt

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,20 +3,25 @@ import './Header.css';
 
 function Header() {
     return (
+      <div>
+        <div className="adress-bar">
+        <div className="adress-text">terzosanitaer@bluewin.ch | Telefon 044 444 44 44  | Öffnungszeiten 07.00 - 17.00</div>
+        </div>        
         <header className="header">
-            <div className="header-logo">
-                <p class="logotext">
-                  Terzo Sanitär
-                </p>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="/">Startseite</a></li>
-                    <li><a href="/">Dienstleistungen</a></li>
-                    <li><a href="/">Kontakt</a></li>
-                </ul>
-            </nav>
+          <div className="header-logo">
+            <p class="logotext">
+              Terzo Sanitär
+            </p>
+          </div>
+          <nav>
+            <ul>
+              <li><a class="nav-text" href="/">Startseite</a></li>
+              <li><a class="nav-text" href="/">Dienstleistungen</a></li>
+              <li><a class="nav-text" href="/">Kontakt</a></li>
+            </ul>
+          </nav>
         </header>
+      </div>
     );
 }
 
