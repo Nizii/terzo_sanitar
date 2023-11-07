f6aed2b9441c651cbb6167f834c807f09eee8be4
Nizii <nizam.oezdemir@gmx.ch>
2023-11-07 16:20:12 +0100

    Menu Button animation hinzugefügt

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -6,13 +6,32 @@ function Header() {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
+  const toggleMenuAnimation = () => {
+    const menu = document.querySelector('.mobile-menu');
+    if (!isMenuOpen) {
+      // Wird ausgeführt, wenn das Menü derzeit geschlossen ist und geöffnet werden soll.
+      setMenuOpen(true); // Ändern Sie den Status sofort, um das Menü zu öffnen.
+      menu.classList.add('open');
+    } else {
+      // Wird ausgeführt, wenn das Menü geöffnet ist und geschlossen werden soll.
+      // Starten Sie die Schließanimation des Menüs und warten Sie auf deren Ende.
+      menu.classList.remove('open');
+      setTimeout(() => {
+        setMenuOpen(false); // Ändern Sie den Status nach der Animation, um das Menü zu schließen.
+      }, 700); // Diese Dauer sollte der Animationsdauer entsprechen.
+    }
+  };
+  
+  
+  
+
   useEffect(() => {
     function handleResize() {
       setIsMobile(window.innerWidth <= 768);
     }
 
     window.addEventListener('resize', handleResize);
-    handleResize(); // Initial check to set the state based on current window size
+    handleResize();
 
     return () => {
       window.removeEventListener('resize', handleResize);
@@ -34,28 +53,34 @@ function Header() {
           <div className="header-logo">
             <p className="logotext">Terzo Sanitär</p>
           </div>
-          {isMobile ? (
-            <button onClick={() => setMenuOpen(!isMenuOpen)} className="hamburger-menu">
+          {isMobile && (
+            <button
+              onClick={toggleMenuAnimation}
+              className={`hamburger-menu ${isMenuOpen ? 'spin-shrink' : ''}`}>
               ☰
             </button>
-          ) : null}
-          <nav className={`${isMobile ? 'mobile-menu' : 'desktop-nav'} ${isMenuOpen ? 'open' : ''}`}>
+          )}
+          <nav className={`${isMobile ? 'mobile-menu' : 'desktop-nav'}`}>
             {isMobile && (
               <>
-              {/* 
+                {/*
                 <div className="mobile-menu-logo">
                   <img src="./logo.png" alt="logo" />
                 </div>
                 */}
-              <button onClick={() => setMenuOpen(false)} className="close-menu">
-                &times;
-              </button>
+                <button
+                  onClick={toggleMenuAnimation}
+                  className="close-menu">
+                  &times;
+                </button>
               </>
             )}
             <ul className='desktop-ul'>
-              <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-              <li><Link to="/kontakt" onClick={() => setMenuOpen(false)}>Kontakt</Link></li>
-              <li><Link to="/about-me" onClick={() => setMenuOpen(false)}>Über mich</Link></li>
+              <li><Link to="/" onClick={toggleMenuAnimation}>Home</Link></li>
+              {/*
+              <li><Link to="/kontakt" onClick={toggleMenuAnimation}>Kontakt</Link></li>
+              */}
+              <li><Link to="/about-me" onClick={toggleMenuAnimation}>Über mich</Link></li>
             </ul>
           </nav>
         </div>
