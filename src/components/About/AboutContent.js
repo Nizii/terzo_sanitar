503a37f5ec6d34b276a8394491a33d1d14885d1b
Nizii <nizam.oezdemir@gmx.ch>
2024-08-29 10:34:47 +0200

    Diverse Anpassungen

--- src/components/About/AboutContent.js
+++ src/components/About/AboutContent.js
@@ -5,36 +5,33 @@ import { FaLinkedin } from 'react-icons/fa';
 const AboutMe = () => {
   return (
     <main className="about-me">
-      <section className="about-image">
-        <h2>Über mich</h2>
-        <div className="about-image">
+      <h2>Über mich</h2>
+      <div className="about-content">
+        <section className="about-image">
           <img src="./meinBild.jpg" alt="Salvatore Terzo" />
-        </div>
-      </section>
-      <section className="about-info">
-        <h2>Wer bin ich?</h2>
-        <p className="about-highlight">Salvatore Terzo</p>
-        <p className="about-highlight">Sanitär Chefmonteur</p>
-        <p className='about-highlight'>
+        </section>
+        <section className="about-info">
+          <p className="about-highlight">Salvatore Terzo</p>
+          <p className="about-highlight">Sanitär Chefmonteur</p>
+          <p className='about-highlight'>
             <a href="https://linkedin.com/in/terzo-salvatore-angelo-2194b520b" className="linkedin-link">
               <FaLinkedin /> LinkedIn Profil
             </a>
           </p>
-
-
-        <p>
-          Mein Name ist Salvatore Terzo, geboren 1988, und Ihre Zufriedenheit ist mein oberstes Ziel.
-          Meine berufliche Laufbahn begann 2008, als ich meine Lehre als Sanitär Monteur EFZ bei der renommierten Firma A. Baltensberger erfolgreich abschloss.
-          Seitdem konnte ich vielfältige Erfahrungen in diversen Bereichen des Sanitärhandwerks sammeln.
-          Egal ob als Bauleiter, bei Wasserschäden, in der Beratung für Umbauten oder hauptsächlich im Service – ich habe stets mein Bestes gegeben, um die Wünsche meiner Kunden zu erfüllen.
-          Mein stetiges Streben nach Perfektion und Weiterentwicklung führte mich zur Weiterbildung als Chefmonteur.
-          Dies war ein bedeutender Schritt in meiner Karriere und diente nicht nur der Vertiefung meines Fachwissens, sondern auch dem Ziel, mich selbstständig zu machen.
-          Heute, als selbstständiger Fachmann, ist es meine Herzensangelegenheit, Ihr Anliegen mit höchster Sorgfalt und Professionalität zu behandeln.
-          Denn erst wenn Sie vollständig zufrieden sind, bin ich es auch. Ich lade Sie herzlich ein, mich bei Fragen oder Anliegen zu kontaktieren.
-          Gemeinsam finden wir die beste Lösung für Ihr Vorhaben.
-          Ich freue mich darauf, Sie bald persönlich zu beraten und zu unterstützen!
-        </p>
-      </section>
+          <p>
+            Mein Name ist Salvatore Terzo, geboren 1988, und Ihre Zufriedenheit ist mein oberstes Ziel.
+            Meine berufliche Laufbahn begann 2008, als ich meine Lehre als Sanitär Monteur EFZ bei der renommierten Firma A. Baltensberger erfolgreich abschloss.
+            Seitdem konnte ich vielfältige Erfahrungen in diversen Bereichen des Sanitärhandwerks sammeln.
+            Egal ob als Bauleiter, bei Wasserschäden, in der Beratung für Umbauten oder hauptsächlich im Service – ich habe stets mein Bestes gegeben, um die Wünsche meiner Kunden zu erfüllen.
+            Mein stetiges Streben nach Perfektion und Weiterentwicklung führte mich zur Weiterbildung als Chefmonteur.
+            Dies war ein bedeutender Schritt in meiner Karriere und diente nicht nur der Vertiefung meines Fachwissens, sondern auch dem Ziel, mich selbstständig zu machen.
+            Heute, als selbstständiger Fachmann, ist es meine Herzensangelegenheit, Ihr Anliegen mit höchster Sorgfalt und Professionalität zu behandeln.
+            Denn erst wenn Sie vollständig zufrieden sind, bin ich es auch. Ich lade Sie herzlich ein, mich bei Fragen oder Anliegen zu kontaktieren.
+            Gemeinsam finden wir die beste Lösung für Ihr Vorhaben.
+            Ich freue mich darauf, Sie bald persönlich zu beraten und zu unterstützen!
+          </p>
+        </section>
+      </div>
     </main>
   );
 };
--- src/components/Main/Content/Content.js
+++ src/components/Main/Content/Content.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import './Content.css';
+import './Content.css'; // Einziger CSS-Import
+import { FaLinkedin } from 'react-icons/fa';
 
-
-function Content() {
+function SinglePage() {
   const [currentImage, setCurrentImage] = useState("1.JPG");
   const [opacity, setOpacity] = useState(1);
 
@@ -20,32 +20,31 @@ function Content() {
       let currentIndex = 0;
 
       const changeImage = () => {
-        // Bild ausblenden
         setOpacity(0);
-    
         setTimeout(() => {
-            // Bild wechseln
             currentIndex = (currentIndex + 1) % images.length;
-            setCurrentImage(images[currentIndex]); // Diese Zeile tatsächlich das Bild aktualisieren
-    
+            setCurrentImage(images[currentIndex]);
             setTimeout(() => {
-                // Bild einblenden
                 setOpacity(1);
-            }, 500); // Kurze Verzögerung, um dem Browser Zeit zu geben, das Bild zu wechseln, bevor es eingeblendet wird
-        }, 1000); // Zeit für das Ausblenden
-    };
+            }, 500);
+        }, 1000);
+      };
     
-    const intervalId = setInterval(changeImage, 5000);
-
+      const intervalId = setInterval(changeImage, 5000);
       return () => {
           clearInterval(intervalId);
       };
   }, []);
 
+  const apiKey = 'YOUR_GOOGLE_MAPS_API_KEY';
+  const mapSrc = `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=Terzo+Sanitär+GmbH,Lindenstrasse+40,8153+Rümlang`;
+
   return (
     <div className="content">
+      {/* Main Image Section */}
       <img src={currentImage} alt="Sanitär-Bild" style={{ opacity: opacity }} className="content-image" />
       <div className="content-inner">
+        {/* Main Content */}
         <h2>Willkommen bei Terzo Sanitär GmbH</h2>
         <p>Terzo Sanitär ist Ihr zuverlässiger Partner für alle sanitären Anforderungen. Seit Jahren bieten wir qualitativ hochwertige Dienstleistungen für unsere geschätzten Kunden. Lassen Sie uns Ihnen helfen, Ihr Traumbad zu gestalten!</p>
         
@@ -58,23 +57,82 @@ function Content() {
         </ul>
 
         <h3>Warum Terzo Sanitär?</h3>
-        <p>Mein Name ist Salvatore Terzo, geboren 1988, und Ihre Zufriedenheit ist mein oberstes Ziel. 
-Meine berufliche Laufbahn begann 2008, als ich meine Lehre als Sanitär Monteur EFZ bei der renommierten Firma A. Baltensberger erfolgreich abschloss. 
-Seitdem konnte ich vielfältige Erfahrungen in diversen Bereichen des Sanitärhandwerks sammeln. 
-Egal ob als Bauleiter, bei Wasserschäden, in der Beratung für Umbauten oder hauptsächlich im Service – ich habe stets mein Bestes gegeben, um die Wünsche meiner Kunden zu erfüllen.
-Mein stetiges Streben nach Perfektion und Weiterentwicklung führte mich zur Weiterbildung als Chefmonteur. 
-Dies war ein bedeutender Schritt in meiner Karriere und diente nicht nur der Vertiefung meines Fachwissens, sondern auch dem Ziel, mich selbstständig zu machen.
-Heute, als selbstständiger Fachmann, ist es meine Herzensangelegenheit, Ihr Anliegen mit höchster Sorgfalt und Professionalität zu behandeln. Denn erst wenn Sie vollständig zufrieden sind, bin ich es auch. Ich lade Sie herzlich ein, mich bei Fragen oder Anliegen zu kontaktieren. 
-Gemeinsam finden wir die beste Lösung für Ihr Vorhaben.
-Ich freue mich darauf, Sie bald persönlich zu beraten und zu unterstützen!</p>
-      
-        <div className="kontakt">
-          
+        <p>Mein Name ist Salvatore Terzo, geboren 1988, und Ihre Zufriedenheit ist mein oberstes Ziel. Meine berufliche Laufbahn begann 2008, als ich meine Lehre als Sanitär Monteur EFZ bei der renommierten Firma A. Baltensberger erfolgreich abschloss. Seitdem konnte ich vielfältige Erfahrungen in diversen Bereichen des Sanitärhandwerks sammeln. Egal ob als Bauleiter, bei Wasserschäden, in der Beratung für Umbauten oder hauptsächlich im Service – ich habe stets mein Bestes gegeben, um die Wünsche meiner Kunden zu erfüllen. Mein stetiges Streben nach Perfektion und Weiterentwicklung führte mich zur Weiterbildung als Chefmonteur. Dies war ein bedeutender Schritt in meiner Karriere und diente nicht nur der Vertiefung meines Fachwissens, sondern auch dem Ziel, mich selbstständig zu machen. Heute, als selbstständiger Fachmann, ist es meine Herzensangelegenheit, Ihr Anliegen mit höchster Sorgfalt und Professionalität zu behandeln. Denn erst wenn Sie vollständig zufrieden sind, bin ich es auch. Ich lade Sie herzlich ein, mich bei Fragen oder Anliegen zu kontaktieren. Gemeinsam finden wir die beste Lösung für Ihr Vorhaben. Ich freue mich darauf, Sie bald persönlich zu beraten und zu unterstützen!</p>
+      </div>
+
+      {/* About Me Section */}
+      <div className="about-me">
+        <h2>Über mich</h2>
+        <div className="about-content">
+          <section className="about-image">
+            <img src="./meinBild.jpg" alt="Salvatore Terzo" />
+          </section>
+          <section className="about-info">
+            <p className="about-highlight">Salvatore Terzo</p>
+            <p className="about-highlight">Sanitär Chefmonteur</p>
+            <p className='about-highlight'>
+              <a href="https://linkedin.com/in/terzo-salvatore-angelo-2194b520b" className="linkedin-link">
+                <FaLinkedin /> LinkedIn Profil
+              </a>
+            </p>
+            <p>Mein Name ist Salvatore Terzo, geboren 1988, und Ihre Zufriedenheit ist mein oberstes Ziel...</p>
+          </section>
         </div>
-      
+      </div>
+
+      {/* Contact Section */}
+      <div className="kontaktseite">
+        <div className='isolation'>
+          <section className="standort">
+            <h2>Adresse</h2>
+            <p className='kontakt-text'>Terzo Sanitär GmbH</p>
+            <p className='kontakt-text'>Lindenstrasse 40</p>
+            <p className='kontakt-text'>8153 Rümlang</p>
+          </section>
+
+          <section className="direktkontakt">
+            <h2>Kontakt</h2>
+            <p className='kontakt-text'>
+              <a href="tel:+41765893331" className="phone-link-about">Telefon +41 76 589 33 31</a>
+            </p>
+            <p className='kontakt-text'>
+              Email terzosanitaer@bluewin.ch 
+            </p>
+            <p className='kontakt-text'>
+              Öffnungszeiten 07.00 - 17.00
+            </p>
+            <p className='kontakt-text'>
+              <a href="https://linkedin.com/in/terzo-salvatore-angelo-2194b520b" className="linkedin-link">
+                <FaLinkedin /> LinkedIn Profil
+              </a>
+            </p>
+          </section>
+        </div>
+        <section className="map">
+          <h2>Standort</h2>
+          <iframe
+            title="Standort"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            loading="lazy"
+            allowFullScreen
+            src={mapSrc}
+          />
+        </section>
+      </div>
+
+      {/* Cooperation and Suppliers Section */}
+      <div className="kooperationen">
+        <h2>Kooperationen und Lieferanten</h2>
+        <ul>
+          <li>Lieferant 1</li>
+          <li>Lieferant 2</li>
+          <li>Kooperationspartner 1</li>
+        </ul>
       </div>
     </div>
   );
 }
 
-export default Content;
+export default SinglePage;
